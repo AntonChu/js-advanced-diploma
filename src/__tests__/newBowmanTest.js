8d2985421609c93649e51e0d9106587f293b4dc3
Anton <antonch92@gmail.com>
2023-06-11 16:03:11 +0200

    Updates

--- src/__tests__/newBowmanTest.js
+++
@@ -1,7 +0,0 @@
-import Bowman from '../js/characters/Bowman';
-
-test('the creation new exemlars of Bowman is available', () => {
-  const hero = new Bowman(1);
-
-  expect(hero.type).toBe('Bowman');
-});
--- src/__tests__/newCharacterTest.js
+++
@@ -1,5 +0,0 @@
-import Character from '../js/Character';
-
-test('the creation new exemlars of Character is fotbidden', () => {
-  expect(() => { Character(1, 'Bowman'); }).toThrowError();
-});
